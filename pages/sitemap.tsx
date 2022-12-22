ac92fa3fc571a2b584e5ff844933dec3e817e419
David Mateo <davidmateo@outlook.com>
2022-12-21 21:25:25 -0400

    refactor

--- pages/sitemap.tsx
+++ pages/sitemap.tsx
@@ -1,71 +1,54 @@
-import { GetServerSideProps } from "next";
-import fs from "fs";
 import sites from "./data.json";
 
-const SITEMAP_URL_LIMIT = 5;
+export async function getServerSideProps({ res }) {
+  // Fetch URLs for sitemap from API
+  const urls = sites.map((u) => u.url);
+  const baseUrl = "https://sitemap-config.vercel.app";
+  // Split URLs into chunks of 50000
+  const chunkSize = 10;
+  const urlChunks = [];
 
-// This function generates a sitemap XML for a given list of URLs
-function generateSitemapXML(urls: string[]) {
-  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
-  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
-  for (const url of urls) {
-    xml += `  <url>\n`;
-    xml += `    <loc>${url}</loc>\n`;
-    xml += `  </url>\n`;
-  }
-  xml += "</urlset>";
-  return xml;
-}
-
-// This function generates a sitemap index XML for a given list of sitemap URLs
-function generateSitemapIndexXML(sitemapUrls: string[]) {
-  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
-  xml += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
-  for (const sitemapUrl of sitemapUrls) {
-    xml += `  <sitemap>\n`;
-    xml += `    <loc>${sitemapUrl}</loc>\n`;
-    xml += `  </sitemap>\n`;
-  }
-  xml += "</sitemapindex>";
-  return xml;
-}
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // Fetch the data for the sitemaps
-  const data = sites;
-
-  // Split the data into chunks for the individual sitemaps
-  const dataChunks = [];
-  for (let i = 0; i < data.length; i += SITEMAP_URL_LIMIT) {
-    dataChunks.push(data.map((u) => u.url).slice(i, i + SITEMAP_URL_LIMIT));
+  for (let i = 0; i < urls.length; i += chunkSize) {
+    urlChunks.push(urls.slice(i, i + chunkSize));
   }
 
-  // Generate the sitemap URLs
-  const sitemapUrls = dataChunks.map((chunk, index) => {
-    // Generate the sitemap XML
-    const sitemapXml = generateSitemapXML(chunk);
+  // Generate sitemap index
+  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
+  <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urlChunks
+      .map((urlsChunk, i) => {
+        return `
+        <sitemap>
+          <loc>${baseUrl}/sitemap${i + 1}.xml</loc>
+        </sitemap>
+      `;
+      })
+      .join("")}
+  </sitemapindex>`;
 
-    // Write the sitemap XML to a file
-    fs.writeFileSync(`public/sitemap-${index}.xml`, sitemapXml);
-
-    // Return the URL for the sitemap
-    return `https://sitemap-config.vercel.app/sitemap-${index}.xml`;
+  // Generate individual sitemaps
+  const sitemaps = urlChunks.map((urlsChunk, i) => {
+    return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urlsChunk
+        .map((url) => {
+          return `
+          <url>
+            <loc>${url}</loc>
+          </url>
+        `;
+        })
+        .join("")}
+    </urlset>`;
   });
 
-  // Generate the sitemap index XML
-  const sitemapIndexXml = generateSitemapIndexXML(sitemapUrls);
-
-  context.res.setHeader("Content-Type", "text/xml");
-  // we send the XML to the browser
-  context.res.write(sitemapIndexXml);
-  context.res.end();
+  // Set header to xml
+  res.setHeader("Content-Type", "application/xml");
+  res.write(sitemapIndex);
+  res.end();
   return {
     props: {}
   };
-};
-
-function SiteMap() {
-  // getServerSideProps will do the heavy lifting
 }
 
-export default SiteMap;
+export default function Sitemap() {}
